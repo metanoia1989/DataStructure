991e7bdc09794d5bcbf6cb79822684f5908fa370
metanoia1989 <sogaxili@gmail.com>
2020-12-06 22:54:55 +0800

    完成计数排序

--- 漫画算法/第4章 排序算法/count_sort.cpp
+++ 漫画算法/第4章 排序算法/count_sort.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <algorithm>
+#include <array>
 
 //***************************************************************************
 // 计数排序的缺点：
 // 1. 当数列最大和最小值差距过大时，需要创建很长的数组，严重浪费空间，时间复杂度也会随之提高    
 // 2. 当数列元素不是整数时，为小数时就无法创建对应的统计数组，无法进行计数排序
 //***************************************************************************
+
+void print_arr(int arr[], int size);
 
 /**
  * 计数排序说明
@@ -68,7 +72,7 @@ void count_sort_shift(int arr[], int size)
     
     // 遍历统计数组，构建排序好的数组
     int index = 0;
-    for (size_t i = 0; i < max+1; i++) {
+    for (size_t i = 0; i < countSize; i++) {
         int count = countArr[i];     
         for (size_t j = 0; j < count; j++) {
             arr[index] = i + min; 
@@ -98,26 +102,34 @@ void count_sort_stable(int arr[], int size)
     int countSize = max - min + 1;
     int *countArr = new int[countSize] {};
     
+
     // 遍历数列，填充统计数组
     for (size_t i = 0; i < size; i++) {
-        countArr[arr[i] - min]++;
+        countArr[arr[i]-min]++;
     }
     
     // 计数数组做变形处理，后面的元素等于前面的元素之和
-    for (size_t i = 0; i < countSize; i++) {
+    for (size_t i = 1; i < countSize; i++) { // 从1开始，因为会减1
         countArr[i] += countArr[i-1];
     }
     
-    // 因为计数数组已经变形了，所以无法值不再是正确的次数了 
+    // 因为计数数组已经变形了，所以索引值不再是正确的次数了 
     // 遍历统计数组，构建排序好的数组
+    // 直接遍历原始数组，然后从计数数组中获取排序好后的索引 
     int index = 0;
-    for (size_t i = 0; i < max+1; i++) {
-        int count = countArr[i];     
-        for (size_t j = 0; j < count; j++) {
-            arr[index] = i + min; 
-            index++;
-        }
+    int *sortedArr = new int[size];
+    for (size_t i = 0; i < size ; i++) {
+        int value = arr[i]; // 元素的值
+        int num = countArr[value-min] - 1; // 排序后的索引
+        sortedArr[num] = value;
+        countArr[value-min]--; 
     }
+
+    // 将数组拷贝回去
+    for (size_t i = 0; i < size ; i++) {
+        arr[i] = sortedArr[i];
+    }
+    delete [] sortedArr;
 }
 
 
@@ -139,15 +151,22 @@ int main(int argc, char const *argv[])
     int size = sizeof(arr) / sizeof(arr[0]);
     print_arr(arr, size);
     count_sort(arr, size);
-    std::cout << "count sort后元素输出：" << std::endl;
+    std::cout << "count sort后-->";
     print_arr(arr, size);
 
     int newarr[] = { 7, 5, 6, 10 };
     int newsize = sizeof(newarr) / sizeof(newarr[0]);
     print_arr(newarr, newsize);
-    count_sort(newarr, newsize);
-    std::cout << "count sort shift后元素输出：" << std::endl;
+    count_sort_shift(newarr, newsize);
+    std::cout << "count sort shift后-->";
     print_arr(newarr, newsize);
+
+    int countarr[] = { 95, 94, 91, 98, 99, 90, 99, 93, 91, 92 };
+    int countsize = sizeof(countarr) / sizeof(countarr[0]);
+    print_arr(countarr, countsize);
+    count_sort_stable(countarr, countsize);
+    std::cout << "count sort stable后-->";
+    print_arr(countarr, countsize);
     return 0;
 }
 
