11779d00656415b69dcf5d5cee52c572b7cd9a32
metanoia1989 <sogaxili@gmail.com>
2021-04-08 23:53:26 +0800

    添加算法之美

---
+++ BeautyAlgorithm/01.从数据到算法/data_type_size.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "bool: " << sizeof(bool) << std::endl;
+    std::cout << "char: " << sizeof(char) << std::endl;
+    std::cout << "short: " << sizeof(short) << std::endl;
+    std::cout << "int: " << sizeof(int) << std::endl;
+    std::cout << "long: " << sizeof(long) << std::endl;
+    std::cout << "float: " << sizeof(float) << std::endl;
+    std::cout << "double: " << sizeof(double) << std::endl;
+
+    return 0;
+}
